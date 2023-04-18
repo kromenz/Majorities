46e164d7be5d82a7da39b44dff1c2210c14be431
DiogoDking77 <diogoabreis@gmail.com>
2023-04-18 16:51:56 +0100

    mini tabuleiro

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ PecasB= []
 
 LinhasDirecao1= [[1,3,5],[2,7,9],[4,6,12], [8,11,14],[10,13,15]]
 PontDirecao1=[0,0]
-Direcao1=[LinhasDirecao1,PontDirecao1]
+VencedorDirecao1=[' ',' ',' ',' ',' ']
+Direcao1=[LinhasDirecao1,PontDirecao1,VencedorDirecao1]
 
 LinhasDirecao2=[[1,2,4],[3,6,8],[5,7,10],[9,11,13],[12,14,15]]
 PontDirecao2=[0,0]
-Direcao2=[LinhasDirecao2,PontDirecao2]
+VencedorDirecao2=[' ',' ',' ',' ',' ']
+Direcao2=[LinhasDirecao2,PontDirecao2,VencedorDirecao2]
 
 LinhasDirecao3=[[4,8,10],[2,6,13],[1,11,15],[3,7,14],[5,9,12]]
 PontDirecao3=[0,0]
-Direcao3=[LinhasDirecao3,PontDirecao3]
+VencedorDirecao3=[' ',' ',' ',' ',' ']
+Direcao3=[LinhasDirecao3,PontDirecao3,VencedorDirecao3]
 
 
 def sair():
@@ -45,31 +48,33 @@ def menu():
 
     return op
 
-def board(player):
-        print(f"                  _____                   |")
-        print(f"                 /     \\                  |")
-        print(f"           _____/   {tabuleiro[0]}   \\_____            |")
-        print(f"          /     \       /     \\           |")
-        print(f"    _____/   {tabuleiro[1]}   \_____/   {tabuleiro[2]}   \\_____     |")
-        print(f"   /     \       / XXX \       /     \\    |")
-        print(f"  /   {tabuleiro[3]}   \_____/ XXXXX \_____/   {tabuleiro[4]}   \\   |")
-        print(f"  \       /     \ XXXXX /     \       /   |")
-        print(f"   \_____/   {tabuleiro[5]}   \_____/   {tabuleiro[6]}   \_____/    |")
-        print(f"   /     \       / XXX \       /     \\    |")
-        print(f"  /   {tabuleiro[7]}   \_____/ XXXXX \_____/   {tabuleiro[8]}   \\   |")
-        print(f"  \       / XXX \ XXXXX / XXX \       /   |")
-        print(f"   \_____/ XXXXX \_____/ XXXXX \_____/    |")
-        print(f"   /     \ XXXXX /     \ XXXXX /     \\    |")
-        print(f"  /  {tabuleiro[9]}   \_____/  {tabuleiro[10]}   \_____/  {tabuleiro[11]}   \\   |")
-        print(f"  \       /     \       /     \       /   |")
-        print(f"   \_____/  {tabuleiro[12]}   \_____/  {tabuleiro[13]}   \_____/    |")
-        print(f"         \       /     \       /          |")
-        print(f"          \_____/  {tabuleiro[14]}   \_____/           |")
-        print(f"                \       /                 |")
-        print(f"                 \_____/                  |")
-        print(f'{Direcao1[1][0]} - {Direcao1[1][1]}')
-        print(f'{Direcao2[1][0]} - {Direcao2[1][1]}')
-        print(f'{Direcao3[1][0]} - {Direcao3[1][1]}')
+def board():
+        print(f"                  _____                   |", end="     \n")
+        print(f"                 /     \\                  |", end="     \n")
+        print(f"           _____/   {tabuleiro[0]}   \\_____            |", end="     \n")
+        print(f"          /     \       /     \\           |", end="     \n")
+        print(f"    _____/   {tabuleiro[1]}   \_____/   {tabuleiro[2]}   \\_____     |", end="     \n")
+        print(f"   /     \       / XXX \       /     \\    |", end="     \n")
+        print(f"  /   {tabuleiro[3]}   \_____/ XXXXX \_____/   {tabuleiro[4]}   \\   |", end="     \n")
+        print(f"  \       /     \ XXXXX /     \       /   |", end="     \n")
+        print(f"   \_____/   {tabuleiro[5]}   \_____/   {tabuleiro[6]}   \_____/    |", end="     \n")
+        print(f"   /     \       / XXX \       /     \\    |", end=f"            {Direcao1[1][0]} - {Direcao1[1][1]}           |           {Direcao2[1][0]} - {Direcao2[1][1]}           |           {Direcao3[1][0]} - {Direcao3[1][1]}     \n")
+        print(f"  /   {tabuleiro[7]}   \_____/ XXXXX \_____/   {tabuleiro[8]}   \\   |", end=f"             ___            |            ___            |            ___                 \n")
+        print(f"  \       / XXX \ XXXXX / XXX \       /   |", end=f"         ___/ {Direcao1[2][0]} \___        |        ___/ {Direcao2[2][0]} \___        |        ___/ {Direcao3[2][2]} \___             \n")
+        print(f"   \_____/ XXXXX \_____/ XXXXX \_____/    |", end=f"     ___/ {Direcao1[2][1]} \___/ {Direcao1[2][0]} \___    |    ___/ {Direcao2[2][0]} \___/ {Direcao2[2][1]} \___    |    ___/ {Direcao3[2][1]} \___/ {Direcao3[2][3]} \___         \n")
+        print(f"   /     \ XXXXX /     \ XXXXX /     \\    |", end=f"    / {Direcao1[2][2]} \___/ {Direcao1[2][1]} \___/ {Direcao1[2][0]} \   |   / {Direcao2[2][0]} \___/ {Direcao2[2][1]} \___/ {Direcao2[2][2]} \   |   / {Direcao3[2][0]} \___/ {Direcao3[2][2]} \___/ {Direcao3[2][4]} \        \n")
+        print(f"  /  {tabuleiro[9]}   \_____/  {tabuleiro[10]}   \_____/  {tabuleiro[11]}   \\   |", end=f"    \___/ {Direcao1[2][2]} \___/ {Direcao1[2][1]} \___/   |   \___/ {Direcao2[2][1]} \___/ {Direcao2[2][2]} \___/   |   \___/ {Direcao3[2][1]} \___/ {Direcao3[2][3]} \___/        \n")
+        print(f"  \       /     \       /     \       /   |", end=f"    / {Direcao1[2][3]} \___/ {Direcao1[2][2]} \___/ {Direcao1[2][1]} \   |   / {Direcao2[2][1]} \___/ {Direcao2[2][2]} \___/ {Direcao2[2][3]} \   |   / {Direcao3[2][0]} \___/ {Direcao3[2][2]} \___/ {Direcao3[2][4]} \        \n")
+        print(f"   \_____/  {tabuleiro[12]}   \_____/  {tabuleiro[13]}   \_____/    |", end=f"    \___/ {Direcao1[2][3]} \___/ {Direcao1[2][2]} \___/   |   \___/ {Direcao2[2][2]} \___/ {Direcao2[2][3]} \___/   |   \___/ {Direcao3[2][1]} \___/ {Direcao3[2][3]} \___/        \n")
+        print(f"         \       /     \       /          |", end=f"    / {Direcao1[2][4]} \___/ {Direcao1[2][3]} \___/ {Direcao1[2][2]} \   |   / {Direcao2[2][2]} \___/ {Direcao2[2][3]} \___/ {Direcao2[2][4]} \   |   / {Direcao3[2][0]} \___/ {Direcao3[2][2]} \___/ {Direcao3[2][4]} \        \n")
+        print(f"          \_____/  {tabuleiro[14]}   \_____/           |", end=f"    \___/ {Direcao1[2][4]} \___/ {Direcao1[2][3]} \___/   |   \___/ {Direcao2[2][3]} \___/ {Direcao2[2][4]} \___/   |   \___/ {Direcao3[2][1]} \___/ {Direcao3[2][3]} \___/        \n")
+        print(f"                \       /                 |", end=f"        \___/ {Direcao1[2][4]} \___/       |       \___/ {Direcao2[2][4]} \___/       |       \___/ {Direcao3[2][2]} \___/            \n")
+        print(f"                 \_____/                  |", end=f"            \___/           |           \___/           |           \___/                \n")
+
+def ContraHumano(player):
+        
+        board()
+ 
         jogada = int(input("Escolha onde quer jogar:"))
 
         while(tabuleiro[jogada-1]=='P' or tabuleiro[jogada-1] == 'B'):
@@ -113,8 +118,12 @@ def ContaDirecao(direcao):
         V=ContaPecas(direcao[0][i])
         if(V=='P'):
               dP += 1
+              direcao[2][i]='P'
         elif(V=='B'):
               dB += 1
+              direcao[2][i]='B'
+        else:
+              direcao[2][i]=' '
         i += 1
 
     direcao[1][0]=dB
@@ -133,7 +142,7 @@ def main():
         p2 = input("\n\tIntroduza o nome do Player 2: ")
         while( end == 0):
             print("\n\t\t" + p1 + " vs " + p2)
-            board(player)
+            ContraHumano(player)
             if(player==True):
                 player=False
             else:
@@ -141,7 +150,7 @@ def main():
             ContaDirecao(Direcao1)
             ContaDirecao(Direcao2)
             ContaDirecao(Direcao3)
-            board(player)
+            ContraHumano(player)
 
     elif(op == 2):
         print("\n\t Humano vs Minimax")
