de89083b6ebfaeb7d38a7d50bf057ea7e34e7ba3
kromenz <rafael.pires.andre@ipvc.pt>
2023-04-20 12:57:25 +0100

    fa

--- jogo/main.py
+++ jogo/main.py
@@ -3,13 +3,6 @@ from games.connect4.players.minimax import MinimaxConnect4Player
 from games.connect4.players.random import RandomConnect4Player
 from games.connect4.simulator import Connect4Simulator
 from games.game_simulator import GameSimulator
-from games.poker.players.always_bet import AlwaysBetKuhnPokerPlayer
-from games.poker.players.always_bet_king import AlwaysBetKingKuhnPokerPlayer
-from games.poker.players.always_pass import AlwaysPassKuhnPokerPlayer
-from games.poker.players.cfr import CFRKuhnPokerPlayer
-from games.poker.players.random import RandomKuhnPokerPlayer
-from games.poker.simulator import KuhnPokerSimulator
-
 
 def run_simulation(desc: str, simulator: GameSimulator, iterations: int):
     print(f"----- {desc} -----")
@@ -56,66 +49,9 @@ def main():
         }
     ]
 
-    poker_simulations = [
-        # uncomment to play as human
-        #{
-        #    "name": "Connect4 - Human VS Random",
-        #    "player1": HumanKuhnPokerPlayer("Human"),
-        #    "player2": RandomKuhnPokerPlayer("Random")
-        #},
-        {
-            "name": "Kuhn Poker - Random VS Random",
-            "player1": RandomKuhnPokerPlayer("Random 1"),
-            "player2": RandomKuhnPokerPlayer("Random 2")
-        },
-        {
-            "name": "Kuhn Poker - AlwaysBet VS Random",
-            "player1": AlwaysBetKuhnPokerPlayer("AlwaysBet"),
-            "player2": RandomKuhnPokerPlayer("Random")
-        },
-        {
-            "name": "Kuhn Poker - AlwaysPass VS Random",
-            "player1": AlwaysPassKuhnPokerPlayer("AlwaysPass"),
-            "player2": RandomKuhnPokerPlayer("Random")
-        },
-        {
-            "name": "Kuhn Poker - AlwaysBet VS AlwaysPass",
-            "player1": AlwaysBetKuhnPokerPlayer("AlwaysBet"),
-            "player2": AlwaysPassKuhnPokerPlayer("AlwaysPass")
-        },
-        {
-            "name": "Kuhn Poker - AlwaysBet VS AlwaysBetKing",
-            "player1": AlwaysBetKuhnPokerPlayer("AlwaysBet"),
-            "player2": AlwaysBetKingKuhnPokerPlayer("AlwaysBetKing")
-        },
-        {
-            "name": "Kuhn Poker - CFR VS Random",
-            "player1": CFRKuhnPokerPlayer("CFR"),
-            "player2": RandomKuhnPokerPlayer("Random")
-        },
-        {
-            "name": "Kuhn Poker - CFR VS AlwaysPass",
-            "player1": CFRKuhnPokerPlayer("CFR"),
-            "player2": AlwaysPassKuhnPokerPlayer("AlwaysPass")
-        },
-        {
-            "name": "Kuhn Poker - CFR VS AlwaysBet",
-            "player1": CFRKuhnPokerPlayer("CFR"),
-            "player2": AlwaysBetKuhnPokerPlayer("AlwaysBet")
-        },
-        {
-            "name": "Kuhn Poker - CFR VS AlwaysBetKing",
-            "player1": CFRKuhnPokerPlayer("CFR"),
-            "player2": AlwaysBetKingKuhnPokerPlayer("AlwaysBetKing")
-        }
-    ]
-
+    
     for sim in c4_simulations:
         run_simulation(sim["name"], Connect4Simulator(sim["player1"], sim["player2"]), num_iterations)
-
-    for sim in poker_simulations:
-        run_simulation(sim["name"], KuhnPokerSimulator(sim["player1"], sim["player2"]), num_iterations)
-
 
 if __name__ == "__main__":
     main()
